5c50cc4b10dc9da50257843d3c7ea942317a1c7b
Jannik Portz <hello@jannikportz.de>
2015-04-06 12:29:49 +0200

    less improvements added datepicker to detail fields hide detail fields on parent sets

--- javascript/opening-hours.js
+++ javascript/opening-hours.js
@@ -241,6 +241,44 @@ jQuery(document).ready(function () {
   jQuery('#op-irregular-openings-wrap').opIOs();
 
 });
+( function ( $ ) {
+
+  $(document).ready( function () {
+
+    var dateStart = $('#op-set-detail-date-start');
+    var dateEnd = $('#op-set-detail-date-end');
+
+    var parent = $('#parent_id');
+
+    if ( parent.val() === "" ) {
+
+      dateStart.parents('.field').hide();
+      dateEnd.parents('.field').hide();
+      $('#op-set-detail-week-scheme-all').parents('.field').hide();
+
+      return;
+    }
+
+    dateStart.addClass('input-gray');
+    dateEnd.addClass('input-gray');
+
+    dateStart.datepicker({
+      dateFormat: 'yy-mm-dd',
+      onClose: function (date) {
+        dateEnd.datepicker("option", "minDate", date);
+      }
+    });
+
+    dateEnd.datepicker({
+      dateFormat: 'yy-mm-dd',
+      onClose: function (date) {
+        dateStart.datepicker("option", "maxDate", date);
+      }
+    });
+
+  });
+
+} )( jQuery );
 (function ($) {
 
   $.fn.opPaypalDonation = function () {
