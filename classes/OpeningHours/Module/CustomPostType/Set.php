2a69a91fe474e30eccddc8c3a44c52df6bfbc9f0
Jannik Portz <hello@jannikportz.de>
2015-12-31 11:58:37 +0100

    code refactoring in Set CPT class

--- classes/OpeningHours/Module/CustomPostType/Set.php
+++ classes/OpeningHours/Module/CustomPostType/Set.php
@@ -1,26 +1,26 @@
 <?php
-/**
- *  Opening Hours: Module: CPT: Set
- */
 
 namespace OpeningHours\Module\CustomPostType;
 
 use OpeningHours\Module\AbstractModule;
 use OpeningHours\Module\I18n;
 
+/**
+ * Set Custom Post Type
+ *
+ * @author      Jannik Portz
+ * @package     OpeningHours\Module\CustomPostType
+ */
 class Set extends AbstractModule {
 
-	/**
-	 *  Constants
-	 */
-	const   CPT_SLUG = 'op-set';
-	const   META_BOX_ID = 'op-set-periods';
-	const   META_BOX_CONTEXT = 'advanced';
-	const   META_BOX_PRIORITY = 'high';
-	const   PERIODS_META_KEY = '_op_set_periods';
-	const   TEMPLATE_META_BOX = 'op-set-meta-box.php';
-	const   NONCE_NAME = 'op-update-set-nonce';
-	const   NONCE_VALUE = 'op-set-opening-hours';
+	const CPT_SLUG = 'op-set';
+	const META_BOX_ID = 'op-set-periods';
+	const META_BOX_CONTEXT = 'advanced';
+	const META_BOX_PRIORITY = 'high';
+	const PERIODS_META_KEY = '_op_set_periods';
+	const TEMPLATE_META_BOX = 'op-set-meta-box.php';
+	const NONCE_NAME = 'op-update-set-nonce';
+	const NONCE_VALUE = 'op-set-opening-hours';
 
 	/**
 	 * Meta Boxes
@@ -28,103 +28,62 @@ class Set extends AbstractModule {
 	 *  key:      string w/ MetaBox identifier
 	 *  value:    MetaBox singleton object
 	 *
-	 * @access      protected
-	 * @type        array
+	 * @var       array
 	 */
 	protected static $metaBoxes;
 
-	/**
-	 *  Constructor
-	 *
-	 * @access       public
-	 */
+	/** Constructor */
 	public function __construct() {
+		$this->registerHookCallbacks();
 
-		self::registerHookCallbacks();
-
-		static::setMetaBoxes( array(
+		static::$metaBoxes = array(
 			'OpeningHours'      => MetaBox\OpeningHours::getInstance(),
 			'Holidays'          => MetaBox\Holidays::getInstance(),
 			'IrregularOpenings' => MetaBox\IrregularOpenings::getInstance(),
 			'Donation'          => MetaBox\Donation::getInstance()
-		) );
-
+		);
 	}
 
-	/**
-	 *  Register Hook Callbacks
-	 *
-	 * @access       public
-	 * @static
-	 */
-	public static function registerHookCallbacks() {
-
-		add_action( 'init', array( __CLASS__, 'registerPostType' ) );
-		add_action( 'admin_menu', array( __CLASS__, 'cleanUpMenu' ) );
-		add_action( 'add_detail_fields', array( __CLASS__, 'registerDetailFields' ) );
-
+	/** Register Hook Callbacks */
+	public function registerHookCallbacks() {
+		add_action( 'init', array( $this, 'registerPostType' ) );
+		add_action( 'admin_menu', array( $this, 'cleanUpMenu' ) );
+		add_action( 'add_detail_fields', array( $this, 'registerDetailFields' ) );
 	}
 
-	/**
-	 *  Register Post Type
-	 *
-	 * @access       public
-	 * @static
-	 * @wp_action    init
-	 */
-	public static function registerPostType() {
-
-		register_post_type( self::CPT_SLUG, self::getArguments() );
-
+	/** Registers Post Type */
+	public function registerPostType() {
+		register_post_type( self::CPT_SLUG, $this->getArguments() );
 	}
 
-	/**
-	 *  Clean Up Menu
-	 *
-	 * @access       public
-	 * @static
-	 * @wp_action    admin_menu
-	 */
-	public static function cleanUpMenu() {
-
+	/** Clean Up Menu */
+	public function cleanUpMenu() {
 		global $submenu;
 
 		/** Top Level: Registered via post_type op-set: Remove "Add New" Item */
 		unset( $submenu['edit.php?post_type=op-set'][10] );
-
 	}
 
-	/**
-	 *  Register Detail Fields
-	 *
-	 * @access       public
-	 * @static
-	 * @wp_action    add_detail_fields
-	 */
-	public static function registerDetailFields() {
-
-		/** Field: Description */
+	/** Registers Detail Fields */
+	public function registerDetailFields () {
 		register_detail_field( self::CPT_SLUG, array(
 			'type'    => 'textarea',
 			'slug'    => 'description',
 			'caption' => __( 'Description', I18n::TEXTDOMAIN )
 		) );
 
-		/** Field: Date Start */
 		register_detail_field( self::CPT_SLUG, array(
 			'type'    => 'text',
 			'slug'    => 'date-start',
 			'caption' => __( 'Date Start', I18n::TEXTDOMAIN )
 		) );
 
-		/** Field: Date End */
 		register_detail_field( self::CPT_SLUG, array(
 			'type'    => 'text',
 			'slug'    => 'date-end',
 			'caption' => __( 'Date End', I18n::TEXTDOMAIN )
 		) );
 
-		/** Field: Week Scheme */
 		register_detail_field( self::CPT_SLUG, array(
 			'type'        => 'radio',
 			'slug'        => 'week-scheme',
@@ -137,25 +96,19 @@ class Set extends AbstractModule {
 			)
 		) );
 
-		/** Field: Child Set Notice */
 		register_detail_field( self::CPT_SLUG, array(
 			'type'        => 'heading',
 			'slug'        => 'child-set-notice',
 			'heading'     => __( 'Add a Child-Set', I18n::TEXTDOMAIN ),
 			'description' => __( 'You may add a child set that overwrites the parent Opening Hours in specific time range. Use the post type hierarchy.', I18n::TEXTDOMAIN )
 		) );
-
 	}
 
 	/**
-	 *  Get Labels
-	 *
-	 * @access       public
-	 * @static
-	 * @return       array
+	 * Getter: Labels
+	 * @return    array
 	 */
-	public static function getLabels() {
-
+	public function getLabels() {
 		return array(
 			'name'               => __( 'Sets', I18n::TEXTDOMAIN ),
 			'singular_name'      => __( 'Set', I18n::TEXTDOMAIN ),
@@ -172,20 +125,15 @@ class Set extends AbstractModule {
 			'not_found'          => __( 'No sets found.', I18n::TEXTDOMAIN ),
 			'not_found_in_trash' => __( 'No sets found in Trash.', I18n::TEXTDOMAIN )
 		);
-
 	}
 
 	/**
-	 *  Get Arguments
-	 *
-	 * @access       public
-	 * @static
-	 * @return       array
+	 * Getter: Arguments
+	 * @return    array
 	 */
-	public static function getArguments() {
-
+	public function getArguments() {
 		return array(
-			'labels'             => self::getLabels(),
+			'labels'             => $this->getLabels(),
 			'public'             => false,
 			'publicly_queryable' => true,
 			'show_ui'            => true,
@@ -198,29 +146,5 @@ class Set extends AbstractModule {
 			'menu_icon'          => 'dashicons-clock',
 			'supports'           => array( 'title', 'custom-fields', 'page-attributes' )
 		);
-
 	}
-
-	/**
-	 * Getter: Meta Boxes
-	 *
-	 * @access        public
-	 * @return        array
-	 */
-	public static function getMetaBoxes() {
-		return static::$metaBoxes;
-	}
-
-	/**
-	 * Setter: Meta Boxes
-	 *
-	 * @access        protected
-	 * @static
-	 *
-	 * @param         array $metaBoxes
-	 */
-	protected static function setMetaBoxes( array $metaBoxes ) {
-		static::$metaBoxes = $metaBoxes;
-	}
-
 }
