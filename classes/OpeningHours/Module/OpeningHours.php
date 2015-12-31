600007f136e8c2a509324144bbbacb460bb66fa5
Jannik Portz <hello@jannikportz.de>
2015-12-31 11:49:35 +0100

    code refactoring in OpeningHours module

--- classes/OpeningHours/Module/OpeningHours.php
+++ classes/OpeningHours/Module/OpeningHours.php
@@ -1,10 +1,8 @@
 <?php
-/**
- *  Opening Hours: Module: Opening Hours
- */
 
 namespace OpeningHours\Module;
 
+use OpeningHours\Entity\Set;
 use OpeningHours\Misc\ArrayObject;
 use OpeningHours\Entity\Set as SetEntity;
 use OpeningHours\Module\CustomPostType\Set as SetCpt;
@@ -12,66 +10,41 @@ use OpeningHours\Module\CustomPostType\Set as SetCpt;
 use WP_Screen;
 use WP_Post;
 
+/**
+ * OpeningHours Module
+ *
+ * @author      Jannik Portz
+ * @package     OpeningHours\Module
+ */
 class OpeningHours extends AbstractModule {
 
 	/**
-	 *  Sets
-	 *
-	 * @access     protected
-	 * @static
-	 * @type       ArrayObject
+	 * Collection of all sets in the system
+	 * @type      ArrayObject
 	 */
 	protected static $sets;
 
 	/**
-	 *  Current Set Id
-	 *
-	 * @access     protected
-	 * @static
-	 * @type       int
+	 * Id of the current Set
+	 * @type      int
 	 */
 	protected static $currentSetId;
 
-	/**
-	 *  Constructor
-	 *
-	 * @access     public
-	 */
+	/** Constructor */
 	public function __construct() {
-
-		self::setSets( new ArrayObject );
-
+		self::$sets = new ArrayObject();
 		self::registerHookCallbacks();
-
 	}
 
-	/**
-	 *  Register Hook Callbacks
-	 *
-	 * @access     public
-	 * @static
-	 */
-	public static function registerHookCallbacks() {
-
-		add_filter( 'detail_fields_metabox_context', array( __CLASS__, 'modifyDetailFieldContext' ) );
-
-		add_action( 'init', array( __CLASS__, 'init' ) );
-		add_action( 'current_screen', array( __CLASS__, 'init_admin' ) );
-
-
+	/** Register Hook Callbacks */
+	public function registerHookCallbacks () {
+		add_filter( 'detail_fields_metabox_context', array( $this, 'modifyDetailFieldContext' ) );
+		add_action( 'init', array( $this, 'init' ) );
+		add_action( 'current_screen', array( $this, 'initAdmin' ) );
 	}
 
-	/**
-	 * Initializer
-	 * initializes all parent posts and loads children
-	 * gets called on every init
-	 *
-	 * @access     public
-	 * @static
-	 * @wp_action  init
-	 */
-	public static function init() {
-
+	/** Initializes all parent posts and loads children */
+	public function init () {
 		// Get all parent op-set posts
 		$posts = get_posts( array(
 			'post_type'   => SetCpt::CPT_SLUG,
@@ -79,198 +52,130 @@ class OpeningHours extends AbstractModule {
 			'numberposts' => - 1
 		) );
 
-		foreach ( $posts as $singlePost ) :
-			self::getSets()->offsetSet( $singlePost->ID, new SetEntity( $singlePost ) );
-		endforeach;
+		foreach ( $posts as $singlePost )
+			self::$sets->offsetSet( $singlePost->ID, new SetEntity( $singlePost ) );
 
 		self::initCurrentSet();
-
 	}
 
 	/**
-	 * Initializer Admin
 	 * Initializes all Set posts for post_type op_set admin screen
 	 * Overwrites Sets that have been set in init()
-	 *
-	 * @access    public
-	 * @static
-	 * @wp_action current_screen
 	 */
-	public static function init_admin() {
-
+	public function initAdmin () {
 		$screen = get_current_screen();
 
-		if ( ! $screen instanceof WP_Screen ) :
+		if ( !$screen instanceof WP_Screen ) {
 			trigger_error( sprintf( '%s::%s(): get_current_screen() may be hooked too early. Return value is not an instance of WP_Screen.', __CLASS__, __METHOD__ ) );
-
-			return;
-		endif;
-
-		/**
-		 * Skip if current screen is no op_set post edit screen
-		 */
-		if ( ! $screen->base == 'post' or ! $screen->post_type == SetCpt::CPT_SLUG ) {
 			return;
 		}
 
-		/**
-		 * Redo Child Set mechanism
-		 */
+		// Skip if current screen is no op_set post edit screen
+		if ( !$screen->base == 'post' or !$screen->post_type == SetCpt::CPT_SLUG )
+			return;
+
+		// Redo Child Set mechanism
 		add_action( SetEntity::WP_ACTION_BEFORE_SETUP, function ( SetEntity $set ) {
-
 			$parent_post = $set->getParentPost();
-
 			$set->setId( $parent_post->ID );
 			$set->setPost( $parent_post );
-
 		} );
 
-		/**
-		 * Load new ArrayObject into sets property
-		 */
-		self::setSets( new ArrayObject );
+		self::$sets = new ArrayObject;
 
 		$posts = get_posts( array(
 			'post_type'   => SetCpt::CPT_SLUG,
 			'numberposts' => - 1
 		) );
 
-		foreach ( $posts as $single_post ) :
+		foreach ( $posts as $single_post )
 			self::getSets()->offsetSet( $single_post->ID, new SetEntity( $single_post ) );
-		endforeach;
 
 		self::initCurrentSet();
-
 	}
 
-	/**
-	 * Init current Set
-	 * checks global posts and sets current set
-	 *
-	 * @access      protected
-	 * @static
-	 */
+	/** Checks global posts and sets current set */
 	protected static function initCurrentSet() {
-
 		global $post;
 
-		if ( ! $post instanceof WP_Post ) {
+		if ( !$post instanceof WP_Post )
 			return;
-		}
 
-		if ( self::getSets()->offsetGet( $post->ID ) instanceof SetEntity ) {
-			self::setCurrentSetId( $post->ID );
-		}
-
+		if ( self::$sets->offsetGet( $post->ID ) instanceof SetEntity )
+			self::$currentSetId = $post->ID;
 	}
 
 	/**
-	 * Modify Detail Field Context
 	 * Forces Detail Fields Meta Box to show up in sidebar
-	 *
-	 * @access     public
-	 * @static
-	 * @return     string
+	 * @return    string
 	 */
-	public static function modifyDetailFieldContext() {
+	public function modifyDetailFieldContext() {
 		return 'side';
 	}
 
 	/**
 	 * Getter: Sets
-	 *
-	 * @access     public
-	 * @static
-	 * @return     ArrayObject
+	 * @return    ArrayObject
 	 */
 	public static function getSets() {
 		return self::$sets;
 	}
 
 	/**
-	 * Get Sets Options
-	 * returns a numeric array with:
+	 * Returns a numeric array with:
 	 *   key:     int with set id
 	 *   value:   string with set name
 	 *
-	 * @access      public
-	 * @static
-	 * @return      array
+	 * @return    array
 	 */
 	public static function getSetsOptions() {
-
 		$sets = array();
-
-		foreach ( self::getSets() as $set ) :
-
+		foreach ( self::getSets() as $set )
 			$sets[ $set->getId() ] = $set->getPost()->post_title;
 
-		endforeach;
-
 		return $sets;
-
 	}
 
 	/**
-	 *  Setter: Sets
+	 * Setter: Sets
 	 *
-	 * @access     protected
-	 * @static
-	 *
-	 * @param      ArrayObject $sets
+	 * @param     ArrayObject $sets
 	 */
 	public static function setSets( ArrayObject $sets ) {
 		self::$sets = $sets;
 	}
 
 	/**
-	 *  Getter: Current Set Id
-	 *
-	 * @access     public
-	 * @static
-	 * @return     int
+	 * Getter: Current Set Id
+	 * @return    int
 	 */
 	public static function getCurrentSetId() {
 		return self::$currentSetId;
 	}
 
 	/**
-	 *  Setter: Current Set Id
-	 *
-	 * @access     public
-	 * @static
-	 *
-	 * @param      int $currentSetId
+	 * Setter: Current Set Id
+	 * @param     int       $currentSetId
 	 */
 	public static function setCurrentSetId( $currentSetId ) {
 		self::$currentSetId = (int) $currentSetId;
 	}
 
 	/**
-	 *  Getter: Set
-	 *
-	 * @access     public
-	 * @static
-	 *
-	 * @param      int $setId
-	 *
-	 * @return     \OpeningHours\Entity\Set
+	 * Getter: Set
+	 * @param     int       $setId
+	 * @return    Set
 	 */
-	public static function getSet( $setId ) {
+	public static function getSet ( $setId ) {
 		return self::getSets()->offsetGet( $setId );
 	}
 
 	/**
-	 *  Getter: Current Set
-	 *
-	 * @access     public
-	 * @static
-	 * @return     \OpeningHours\Entity\Set
+	 * Getter: Current Set
+	 * @return     Set
 	 */
 	public static function getCurrentSet() {
 		$setId = self::getCurrentSetId();
-
 		return self::getSet( $setId );
 	}
-
 }
