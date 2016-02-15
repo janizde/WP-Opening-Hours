1c23e030fd42e6029e675adb9305ae9d56d7e11f
Jannik Portz <hello@jannikportz.de>
2016-02-15 23:52:26 +0100

    removed unused method ArrayObject::exchangeElement

--- classes/OpeningHours/Util/ArrayObject.php
+++ classes/OpeningHours/Util/ArrayObject.php
@@ -25,25 +25,6 @@ class ArrayObject extends NativeArrayObject {
 	}
 
 	/**
-	 * Exchanges old element with new element.
-	 *
-	 * @param     mixed     $oldElement The old element to be replaced
-	 * @param     mixed     $newElement The element that the old element should be replaced with
-	 *
-	 * @return    bool      Whether old element has been found or not
-	 */
-	public function exchangeElement( $oldElement, $newElement ) {
-		foreach ( $this as $id => $current ) {
-			if ( $current === $oldElement ) {
-				$this->offsetUnset( $id );
-				$this->offsetSet( $id, $newElement );
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
 	 * Creates a new ArrayObjects and fills is with the provided data
 	 *
 	 * @param     array     $data     The data to fill the ArrayObject with
