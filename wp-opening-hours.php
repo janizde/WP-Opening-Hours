f312d79c41aaeab0dda1be24dc50336fb9d52849
Jannik Portz <hello@jannikportz.de>
2016-07-24 01:03:25 +0200

    require detail fields

--- wp-opening-hours.php
+++ wp-opening-hours.php
@@ -15,7 +15,8 @@ define( 'OP_NAME', 'Opening Hours' );
 define( 'OP_REQUIRED_PHP_VERSION', '5.3' );
 define( 'OP_REQUIRED_WP_VERSION', '3.1' );
 
-require_once( 'includes/admin-notice-helper/admin-notice-helper.php' );
+require_once 'includes/admin-notice-helper/admin-notice-helper.php';
+require_once 'lib/detail-fields.php';
 
 function op_admin_notice_php () {
 	$string = __( 'Plugin Opening Hours requires at least PHP Version %s. Your Installation of WordPress is currently running on PHP %s', 'opening-hours' );
