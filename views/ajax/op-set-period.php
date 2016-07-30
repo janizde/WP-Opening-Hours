56c0e80a32de595e98a969446f7a85ec9f402aad
Jannik Portz <hello@jannikportz.de>
2016-07-30 16:18:28 +0200

    always use standard time format in op-set-period

--- views/ajax/op-set-period.php
+++ views/ajax/op-set-period.php
@@ -1,10 +1,9 @@
 <?php
-/** @var Period $period */
 use OpeningHours\Entity\Period;
 use OpeningHours\Util\Dates;
 
+/** @var Period $period */
 $period = $this->data['period'];
-$timeFormat = Dates::getTimeFormat();
 ?>
 
 <tr class="period">
@@ -14,7 +13,7 @@ $timeFormat = Dates::getTimeFormat();
 			name="opening-hours[<?php echo $period->getWeekday(); ?>][start][]"
 			type="text"
 			class="input-timepicker input-time-end"
-			value="<?php echo $period->getTimeStart()->format( $timeFormat ); ?>"/>
+			value="<?php echo $period->getTimeStart()->format( Dates::STD_TIME_FORMAT ); ?>"/>
 	</td>
 
 	<td class="col-time-end">
@@ -22,7 +21,7 @@ $timeFormat = Dates::getTimeFormat();
 			name="opening-hours[<?php echo $period->getWeekday(); ?>][end][]"
 			type="text"
 			class="input-timepicker input-time-end"
-			value="<?php echo $period->getTimeEnd()->format( $timeFormat ); ?>"/>
+			value="<?php echo $period->getTimeEnd()->format( Dates::STD_TIME_FORMAT ); ?>"/>
 	</td>
 
 	<td class="col-delete-period">
