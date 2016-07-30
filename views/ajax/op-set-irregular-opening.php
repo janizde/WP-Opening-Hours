3dbcb2123aa9d801d6c7c2b9860cb09b46856a50
Jannik Portz <hello@jannikportz.de>
2016-07-30 16:18:42 +0200

    refactored template for irregular opening

--- views/ajax/op-set-irregular-opening.php
+++ views/ajax/op-set-irregular-opening.php
@@ -1,44 +1,37 @@
 <?php
-/**
- * Opening Hours: Views: Ajax: OP Set Holiday
- */
 
-use OpeningHours\Entity\IrregularOpening;
 use OpeningHours\Module\CustomPostType\MetaBox\IrregularOpenings as MetaBox;
-use OpeningHours\Module\I18n;
 use OpeningHours\Util\Dates;
 
-/**
- * pre-defined variables
- *
- * @var         $io        IrregularOpening object
- */
-
-$name      = $io->getName();
-$date      = ( $io->isDummy() ) ? null : $io->getDate()->format( Dates::STD_DATE_FORMAT );
+/** @var \OpeningHours\Entity\IrregularOpening $io */
+$name = $io->getName();
+$date = ( $io->isDummy() ) ? null : $io->getDate()->format( Dates::STD_DATE_FORMAT );
 $timeStart = ( $io->isDummy() ) ? null : $io->getTimeStart()->format( Dates::STD_TIME_FORMAT );
-$timeEnd   = ( $io->isDummy() ) ? null : $io->getTimeEnd()->format( Dates::STD_TIME_FORMAT );
+$timeEnd = ( $io->isDummy() ) ? null : $io->getTimeEnd()->format( Dates::STD_TIME_FORMAT );
+?>
 
-echo '<tr class="op-irregular-opening">';
+<tr class="op-irregular-opening">
+  <td class="col-name">
+    <input type="text" class="widefat name"
+           name="<?php echo MetaBox::POST_KEY; ?>[name][]" value="<?php echo $name; ?>">
+  </td>
 
-echo '<td class="col-name">';
-echo '<input type="text" name="' . MetaBox::POST_KEY . '[name][]" class="widefat name" value="' . $name . '" />';
-echo '</td>';
+  <td class="col-date">
+    <input type="text" class="widefat date input gray"
+           name="<?echo MetaBox::POST_KEY; ?>[date][]" value="<?php echo $date; ?>">
+  </td>
 
-echo '<td class="col-date">';
-echo '<input type="text" name="' . MetaBox::POST_KEY . '[date][]" class="widefat date input-gray" value="' . $date . '" />';
-echo '</td>';
+  <td class="col-time-start">
+    <input type="text" class="widefat time-start input-timepicker input-gray"
+           name="<?php echo MetaBox::POST_KEY; ?>[timeStart][]" value="<?php echo $timeStart; ?>">
+  </td>
 
-echo '<td class="col-time-start">';
-echo '<input type="text" name="' . MetaBox::POST_KEY . '[timeStart][]" class="widefat time-start input-timepicker input-gray" value="' . $timeStart . '" />';
-echo '</td>';
+  <td class="col-time-end">
+    <input type="text" class="widefat time-end input-timepicker input-gray"
+           name="<?php echo MetaBox::POST_KEY; ?>[timeEnd][]" value="<?php echo $timeEnd; ?>">
+  </td>
 
-echo '<td class="col-time-end">';
-echo '<input type="text" name="' . MetaBox::POST_KEY . '[timeEnd][]" class="widefat time-end input-timepicker input-gray" value="' . $timeEnd . '" />';
-echo '</td>';
-
-echo '<td class="col-remove">';
-echo '<button class="button button-remove remove-io has-icon"><i class="dashicons dashicons-no-alt"></i></button>';
-echo '</td>';
-
-echo '</tr>';
+  <td class="col-remove">
+    <button class="button button-remove remove-io has-icon"><i class="dashicons dashicons-no-alt"></i></button>
+  </td>
+</tr>
