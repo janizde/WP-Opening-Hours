7c3677758cc9ba508a454b2c3be59f89b443d069
Jannik Portz <hello@jannikportz.de>
2017-06-12 21:07:15 +0200

    increased version tag

--- classes/OpeningHours/OpeningHours.php
+++ classes/OpeningHours/OpeningHours.php
@@ -31,7 +31,7 @@ class OpeningHours extends AbstractModule {
   protected $widgets;
 
   /** The plugin version */
-  const VERSION = '2.1.1';
+  const VERSION = '2.1.2';
 
   /** The Plugin DB version */
   const DB_VERSION = 2;
