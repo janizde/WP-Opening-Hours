399ff99894efddb70eeb7ca09cc84e8248286829
Jannik Portz <hello@jannikportz.de>
2020-01-04 20:46:14 +0100

    prettify JS

--- assets/scripts/ExtendedSettings.js
+++ assets/scripts/ExtendedSettings.js
@@ -1,29 +1,31 @@
-(function ($) {
-  $.fn.opExtendedSettings = function () {
-    return this.each(function () {
+(function($) {
+  $.fn.opExtendedSettings = function() {
+    return this.each(function() {
       var wrap = $(this);
-      var container = wrap.find('.settings-container');
-      var toggle = wrap.find('.collapse-toggle');
+      var container = wrap.find(".settings-container");
+      var toggle = wrap.find(".collapse-toggle");
 
-      var hidden = container.hasClass('hidden');
+      var hidden = container.hasClass("hidden");
 
-      toggle.click(function () {
+      toggle.click(function() {
         hidden = !hidden;
         if (hidden) {
-          container.addClass('hidden');
+          container.addClass("hidden");
           toggle.html(openingHoursData.translations.moreSettings);
         } else {
-          container.removeClass('hidden');
+          container.removeClass("hidden");
           toggle.html(openingHoursData.translations.fewerSettings);
         }
       });
     });
   };
 
-  $(document).ready(function () {
-    $('.extended-settings').opExtendedSettings();
-    $(document).on('widget-updated widget-added', function (e, widget) {
-      $(widget).find('.extended-settings').opExtendedSettings();
+  $(document).ready(function() {
+    $(".extended-settings").opExtendedSettings();
+    $(document).on("widget-updated widget-added", function(e, widget) {
+      $(widget)
+        .find(".extended-settings")
+        .opExtendedSettings();
     });
   });
 })(jQuery);
--- assets/scripts/Holidays.js
+++ assets/scripts/Holidays.js
@@ -3,15 +3,14 @@
  */
 
 /** Holidays Meta Box */
-jQuery.fn.opHolidays = function () {
-
+jQuery.fn.opHolidays = function() {
   var wrap = jQuery(this);
 
-  var holidaysWrap = wrap.find('tbody');
-  var addButton = wrap.find('.add-holiday');
+  var holidaysWrap = wrap.find("tbody");
+  var addButton = wrap.find(".add-holiday");
 
   function init() {
-    holidaysWrap.find('tr.op-holiday').each(function (index, element) {
+    holidaysWrap.find("tr.op-holiday").each(function(index, element) {
       jQuery(element).opSingleHoliday();
     });
   }
@@ -19,93 +18,86 @@ jQuery.fn.opHolidays = function () {
   init();
 
   function add() {
-
     var data = {
-      'action': 'op_render_single_dummy_holiday'
+      action: "op_render_single_dummy_holiday"
     };
 
-    jQuery.post(ajax_object.ajax_url, data, function (response) {
+    jQuery.post(ajax_object.ajax_url, data, function(response) {
       var newHoliday = jQuery(response).clone();
 
       newHoliday.opSingleHoliday();
 
       holidaysWrap.append(newHoliday);
     });
-
   }
 
-  addButton.click(function (e) {
+  addButton.click(function(e) {
     e.preventDefault();
 
     add();
   });
-
 };
 
 /** Holiday Item */
-jQuery.fn.opSingleHoliday = function () {
-
+jQuery.fn.opSingleHoliday = function() {
   var wrap = jQuery(this);
 
   if (wrap.length > 1) {
-    wrap.each(function (index, element) {
+    wrap.each(function(index, element) {
       jQuery(element).opSingleHoliday();
     });
 
     return;
   }
 
-  var removeButton = wrap.find('.remove-holiday');
-  var inputDateStart = wrap.find('input.date-start');
-  var inputDateEnd = wrap.find('input.date-end');
+  var removeButton = wrap.find(".remove-holiday");
+  var inputDateStart = wrap.find("input.date-start");
+  var inputDateEnd = wrap.find("input.date-end");
 
   function remove() {
     wrap.remove();
   }
 
-  removeButton.click(function (e) {
+  removeButton.click(function(e) {
     e.preventDefault();
 
     remove();
   });
 
   inputDateStart.datepicker({
-    dateFormat: 'yy-mm-dd',
+    dateFormat: "yy-mm-dd",
     firstDay: openingHoursData.startOfWeek || 0,
     dayNames: openingHoursData.weekdays.full,
     dayNamesMin: openingHoursData.weekdays.short,
     dayNamesShort: openingHoursData.weekdays.short,
-    onClose: function (date) {
+    onClose: function(date) {
       inputDateEnd.datepicker("option", "minDate", date);
     }
   });
 
   inputDateEnd.datepicker({
-    dateFormat: 'yy-mm-dd',
+    dateFormat: "yy-mm-dd",
     firstDay: openingHoursData.startOfWeek || 0,
     dayNames: openingHoursData.weekdays.full,
     dayNamesMin: openingHoursData.weekdays.short,
     dayNamesShort: openingHoursData.weekdays.short,
-    onClose: function (date) {
+    onClose: function(date) {
       inputDateStart.datepicker("option", "maxDate", date);
     }
   });
 
-  inputDateStart.focus( function () {
+  inputDateStart.focus(function() {
     inputDateStart.blur();
-  } );
+  });
 
-  inputDateEnd.focus( function () {
+  inputDateEnd.focus(function() {
     inputDateEnd.blur();
-  } );
-
+  });
 };
 
 /**
  * Mapping
  */
-jQuery(document).ready(function () {
-
-  jQuery('#op-holidays-wrap').opHolidays();
-
+jQuery(document).ready(function() {
+  jQuery("#op-holidays-wrap").opHolidays();
 });
